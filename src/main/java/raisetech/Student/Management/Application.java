80f8ed801c50e60a4d47c4d7a6bb827ffa6c8f5a
momochi <niwatorigomi@gmail.com>
2026-01-31 17:16:16 +0900

    dependenciesの追加

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -1,5 +1,6 @@
 package raisetech.Student.Management;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ public class Application {
 
 	@GetMapping("/hello")
 	public String hello(){
+
 		return "Hello, World!";
 	}
 
