82a3e115aadb390db4c57a19a259fd3e0477eae1
momochi <niwatorigomi@gmail.com>
2026-02-03 21:12:38 +0900

    Map形式

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -1,23 +1,43 @@
 package raisetech.Student.Management;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private String name = "Yoshida Saori";
+	private String age = "30";
+
+	private Map<String, String> student = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
+	@GetMapping("/student")
+	public Map<String, String> getStudent() {
+		student.put("name", name);
+		student.put("age", age);
+		return student;
+	}
 
-		return "Hello, World!";
+	@PostMapping("/student")
+	public String postStudent(
+			@RequestParam String name,
+			@RequestParam String age
+	) {
+		this.name = name;
+		this.age = age;
+		return "登録";
 	}
 
 }
