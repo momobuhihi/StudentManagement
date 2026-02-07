f976fea99b4e19e07039fa664b48c966a78b41b8
momochi <niwatorigomi@gmail.com>
2026-02-07 20:36:24 +0900

    DBの名前をListに格納バージョン

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -1,11 +1,12 @@
 package raisetech.Student.Management;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,31 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	private String name = "Yoshida Saori";
-	private String age = "30";
+	@Autowired
+	private StudentRepository repository;
 
-	private Map<String, String> student = new HashMap<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@GetMapping("/student")
-	public Map<String, String> getStudent() {
-		student.put("name", name);
-		student.put("age", age);
-		return student;
+	public List<Student> getStudents() {
+    return repository.findAll();
 	}
 
 
 	@PostMapping("/student")
-	public String postStudent(
-			@RequestParam String name,
-			@RequestParam String age
-	) {
-		this.name = name;
-		this.age = age;
-		return "登録";
+	public void registerStudent(String name, int age) {
+		repository.registerStudent(name, age);
 	}
 
+	@PatchMapping("/student")
+	public void updateStudent(String name, int age){
+repository.updateStudent(name, age);
+	}
+
+	@DeleteMapping("/student")
+	public void deleteStudent(String name){
+repository.deleteStudent(name);
+	}
 }
