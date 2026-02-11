4b4b6c65936e59932744b60206b64bb67aa620f1
momochi <niwatorigomi@gmail.com>
2026-02-11 10:32:23 +0900

    キャメルケースの慣例のためメソッド名変更

--- src/main/java/raisetech/Student/Management/StudentRepository.java
+++ src/main/java/raisetech/Student/Management/StudentRepository.java
@@ -11,7 +11,7 @@ public interface StudentRepository {
   List<Student> search();
 
   @Select("SELECT * FROM students_courses")
-  List<Course> findAllcourses();
+  List<Course> findAllCourses();
 
 
 }
